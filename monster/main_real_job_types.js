edb11082f0e10e3ce43ee89236396b9657eb9eee
Shuwen Wang <shuwen.wang@talentneuron.com>
2025-02-02 22:42:57 -0500

    playwright works for downloading.

--- monster/main_real_job_types.js
+++ monster/main_real_job_types.js
@@ -41,7 +41,7 @@ async function test() {
         customConfig: {
             userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
         },
-        turnstile: false,
+        turnstile: true,
         connectOption: {},
         disableXvfb: false,
         ignoreAllFlags: false
